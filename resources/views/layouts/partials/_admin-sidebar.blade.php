55434fa5a6fab230de9fff7b793e927468d907e7
Aadhar gaur <aadhar41@gmail.com>
2026-03-20 10:51:15 +0530

    feat: add initial admin sidebar layout and a dashboard screenshot.

--- resources/views/layouts/partials/_admin-sidebar.blade.php
+++ resources/views/layouts/partials/_admin-sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('home') }}" class="brand-link">
         <img src="{{ asset('images/ag-logo-small.png') }}" alt="Admin Logo" class="brand-image img-circle elevation-3">
-        <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+        <span class="brand-text font-weight-light" style="font-size: 1.1rem;">Society ERP</span>
     </a>
 
     <div class="sidebar">
